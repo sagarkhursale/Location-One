944e4a4dd952a5b4cfc37174bf2e7ba88e29b0c4
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-10-11 16:35:52 +0530

    GoogleApiClient and callbacks

--- app/src/main/java/com/sagar/location_1/MainActivity.java
+++ app/src/main/java/com/sagar/location_1/MainActivity.java
@@ -2,12 +2,33 @@ package com.sagar.location_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private TextView text_Output;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGoogleApiClient=new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
     }
+
+
+    // END
 }
