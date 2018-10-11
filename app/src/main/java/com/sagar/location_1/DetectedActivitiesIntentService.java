5b056cc1c415eea556e808cd1bc75bbca1774ef0
sagar khursale <sagar.khursale@webmatrixtech.com>
2018-10-11 21:37:46 +0530

    DetectedActivites IntentService

---
+++ app/src/main/java/com/sagar/location_1/DetectedActivitiesIntentService.java
@@ -0,0 +1,25 @@
+package com.sagar.location_1;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+
+public class DetectedActivitiesIntentService extends IntentService {
+
+    private static final String TAG="detection_is";
+
+
+    public DetectedActivitiesIntentService() {
+        super(TAG);
+    }
+
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+    }
+
+
+    //
+}
